6c14fcfb38f00703fd9c74feb9b2f81ee8886b31
wael <78425538+wa20@users.noreply.github.com>
2021-09-16 18:25:31 +0100

    CLIENT: homepage created added temp footer

---
+++ src/pages/Home/homePage.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { createMedia } from '@artsy/fresnel'
+import Footer from "../../components/Footer/footer"
+
+// import {
+//     Button,
+//     Container,
+//     Header,
+//     Icon,
+//     Segment,
+//     Sidebar,
+//     Visibility,
+//     Grid,
+//   } from 'semantic-ui-react'
+
+
+
+  const Home = () => {
+    return (
+      <div>
+
+
+
+          <Footer />
+
+  
+      </div>
+    );
+  };
+  
+  export default Home;
