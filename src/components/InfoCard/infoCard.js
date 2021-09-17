8fec0830b9e330e7b9cca9f4147a2f030b1d3be4
wael <78425538+wa20@users.noreply.github.com>
2021-09-17 16:27:06 +0100

    COMPONENT: basic info card created

---
+++ src/components/InfoCard/infoCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const InfoCard = ({place}) => {
+
+    return (
+        <div>
+
+            <h1>{place.name}</h1>
+
+
+        </div>
+    );
+} 
+
+export default InfoCard
