354c0b668a3d428d61289f3ead555143428c7d6b
wael <78425538+wa20@users.noreply.github.com>
2021-09-17 15:07:54 +0100

    PAGES: minor styling amends to homepage added padding

--- src/pages/Home/homePage.js
+++ src/pages/Home/homePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { createMedia } from "@artsy/fresnel";
 import Map from "../../components/Map/map";
+import List from "../../components/List/list"
 import Footer from "../../components/Footer/footer";
 import { CssBaseline, Grid } from "@material-ui/core";
 
@@ -14,16 +15,15 @@ const Home = () => {
   return (
     <div>
     
-    <Container style={{paddingTop:"3em", paddingBottom:"3em", margin:"0"}}>
-      <Grid container  style={{ width: "100%" }}>
+    {/* <Container style={{paddingTop:"3em", paddingBottom:"3em", margin:"0"}}> */}
+      <Grid container  style={{ width: "100%" }} style={{padding:"2em"}}>
         <Grid item xs={12} md={4}>
-          
-        </Grid>
+          <List/>        </Grid>
         <Grid item xs={12} md={8} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
         <Map />
         </Grid>
       </Grid>
-    </Container>
+    {/* </Container> */}
 
       <Footer />
     </div>
