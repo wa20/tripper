7846204d7fd4898dddf092dc07c7281a776c8b4a
wael <78425538+wa20@users.noreply.github.com>
2021-09-17 15:05:07 +0100

    COMPONENT: added inital choice options for results list section

--- src/components/List/list.js
+++ src/components/List/list.js
@@ -11,20 +11,40 @@ import {
 
 import useStyles from "./listStyles";
 
-
 const List = () => {
-    
-    const classes = useStyles();
-  
-  
-  
-    return (
-      <div className={classes.container}>
-           <Typography variant="h4">Food & Dining around you</Typography>
+  const [type, setType] = useState("hotels");
+  const [rating, setRating] = useState('all')
 
-       
-      </div>
-    );
-  };
-  
-  export default List;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h5">what the tripper?</Typography>
+
+      <FormControl className={classes.formControl}>
+        <InputLabel >Type</InputLabel>
+        <Select value={type} onChange={(event) => setType(event.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+
+      <FormControl className={classes.formControl} >
+        <InputLabel >Rating</InputLabel>
+        <Select value={rating} onChange={(event) => setRating(event.target.value)}>
+          <MenuItem value="all">All</MenuItem>
+          <MenuItem value="3"> Rating 3+</MenuItem>
+          <MenuItem value="3.5">Rating 3.5+</MenuItem>
+          <MenuItem value="4">Rating 4+</MenuItem>
+          <MenuItem value="4.5">Rating 4.5+</MenuItem>
+        </Select>
+      </FormControl>
+
+
+
+    </div>
+  );
+};
+
+export default List;
