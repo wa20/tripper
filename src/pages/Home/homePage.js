ea982203439a14dc0ff5c9fd2596b02b57329c1c
wael <78425538+wa20@users.noreply.github.com>
2021-09-18 11:08:47 +0100

    PAGES: added useeffect and usestate to update map coords, added api import to fetch api calls, added list section to homepage and amended map tag

--- src/pages/Home/homePage.js
+++ src/pages/Home/homePage.js
@@ -1,29 +1,57 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { createMedia } from "@artsy/fresnel";
 import Map from "../../components/Map/map";
-import List from "../../components/List/list"
+import List from "../../components/List/list";
 import Footer from "../../components/Footer/footer";
 import { CssBaseline, Grid } from "@material-ui/core";
+import { getData } from "../../api/index";
 
-import {
-    Container,
-    Header,
-    Segment,
-  } from 'semantic-ui-react'
+import { Container, Header, Segment } from "semantic-ui-react";
 
 const Home = () => {
+  const [places, setPlaces] = useState([]);
+  const [coords, setCoords] = useState({})
+  const [boundary, setBoundary] = useState(null)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+      setCoords({ lat: latitude, lng: longitude });
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(coords, boundary)
+    getData().then((data) => {
+        console.log(data)
+        setPlaces(data)
+    })
+  }, []);
+
   return (
     <div>
-    
-    {/* <Container style={{paddingTop:"3em", paddingBottom:"3em", margin:"0"}}> */}
-      <Grid container  style={{ width: "100%" }} style={{padding:"2em"}}>
+      {/* <Container style={{paddingTop:"3em", paddingBottom:"3em", margin:"0"}}> */}
+      <Grid container style={{ width: "100%" }} style={{ padding: "2em" }}>
         <Grid item xs={12} md={4}>
-          <List/>        </Grid>
-        <Grid item xs={12} md={8} style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <Map />
+          <List places={places}/>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Map 
+           setCoords={setCoords}
+           setBoundary={setBoundary}
+           coords={coords}
+          />
         </Grid>
       </Grid>
-    {/* </Container> */}
+      {/* </Container> */}
 
       <Footer />
     </div>
