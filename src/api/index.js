673bd479ecb461dd10bd4b431080d6fe8b8f0e33
wael <78425538+wa20@users.noreply.github.com>
2021-09-23 18:11:04 +0100

    API: updated static location values to be dynamic and updated based on user location

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-require("dotenv").config();
+// require("dotenv").config();
 
 const URL =
   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
@@ -10,24 +10,26 @@ const URL =
 export const getData = async (sw, ne) => {
   try {
     const {data: { data }} = await axios.get(URL, {
+        
         params: {
-          bl_latitude: sw.lat,
-          tr_latitude: ne.lat,
-          bl_longitude: sw.lng,
-          tr_longitude: ne.lng,
-          limit: "30",
+            bl_latitude: sw.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+            tr_latitude: ne.lat,
+          limit: "20",
         },
+        
         headers: {
           "x-rapidapi-host": process.env.REACT_APP_RAPID_API_HOST,
           "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
         },
       })
-
+      console.log('***Return***',data)
     return data
-
+    
 
   } catch (error) {
-      console.log(error)
+      console.log('***Error***',error)
   }
 };
 
