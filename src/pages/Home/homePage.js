9ce3ef64f13137899eaaab90eb3d02243fbd4920
wael <78425538+wa20@users.noreply.github.com>
2021-09-23 18:21:09 +0100

    PAGES: updated useeffect and usestatem, to geotag loation and auto update user location

--- src/pages/Home/homePage.js
+++ src/pages/Home/homePage.js
@@ -1,38 +1,45 @@
-import React, { Component, useEffect, useState } from "react";
+import React, {useState,  useEffect } from "react";
 import { createMedia } from "@artsy/fresnel";
-import Map from "../../components/Map/map";
-import List from "../../components/List/list";
-import Footer from "../../components/Footer/footer";
+
 import { CssBaseline, Grid } from "@material-ui/core";
 import { getData } from "../../api/index";
+import Maps from "../../components/Map/map";
+import List from "../../components/List/list";
+import Footer from "../../components/Footer/footer";
+
+
 
 import { Container, Header, Segment } from "semantic-ui-react";
 
 const Home = () => {
+const [type, setType] = useState('restaurants');
   const [places, setPlaces] = useState([]);
-  const [coords, setCoords] = useState({})
-  const [boundary, setBoundary] = useState(null)
+  const [coordinates, setCoordinates] = useState({lat: 0, lng: 0 });
+  const [bounds, setBounds] = useState({});
+
+   
+ useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
+          setCoordinates({ lat: latitude, lng: longitude });
+        });
+      }, []);
+
 
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
-      setCoords({ lat: latitude, lng: longitude });
+    console.log("coords & bounds", coordinates, bounds);
+    getData(bounds.sw, bounds.ne)
+        .then((data) => {
+            console.log("getData", data);
+                setPlaces(data);
     });
-  }, []);
-
-  useEffect(() => {
-    console.log(coords, boundary)
-    getData().then((data) => {
-        console.log(data)
-        setPlaces(data)
-    })
-  }, []);
+  }, [coordinates, bounds]);
 
   return (
     <div>
       {/* <Container style={{paddingTop:"3em", paddingBottom:"3em", margin:"0"}}> */}
-      <Grid container style={{ width: "100%" }} style={{ padding: "2em" }}>
+      <Grid container style={{ width: "100%",  padding: "2em"  }}>
         <Grid item xs={12} md={4}>
-          <List places={places}/>
+          <List places={places} />
         </Grid>
         <Grid
           item
@@ -44,10 +51,10 @@ const Home = () => {
             alignItems: "center",
           }}
         >
-          <Map 
-           setCoords={setCoords}
-           setBoundary={setBoundary}
-           coords={coords}
+          <Maps
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
           />
         </Grid>
       </Grid>
