d9948967062905fe059c11cbadfcb5e6330e38ab
wael <78425538+wa20@users.noreply.github.com>
2021-09-23 21:35:16 +0100

    COMPONENT: initial info card wireframe created

--- src/components/InfoCard/infoCard.js
+++ src/components/InfoCard/infoCard.js
@@ -1,15 +1,82 @@
 import React from 'react';
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip } from '@material-ui/core';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import PhoneIcon from '@material-ui/icons/Phone';
+import Rating from '@material-ui/lab/Rating';
+import { styled } from '@mui/material/styles';
+
+import CardHeader from '@mui/material/CardHeader';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import useStyles from './infoCardStyle';
+
+const placeholder = require('../../assets/ResPlaceholder.jpeg')
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
 
 
 const InfoCard = ({place}) => {
 
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+      setExpanded(!expanded);
+    };
+
+    const classes = useStyles();
+
     return (
-        <div>
+        <Card elevation={5}>
+            <CardMedia
+            style={{ height: 350 }}
+            image={place.photo ? place.photo.images.large.url : {placeholder}}
+            title={place.name}
+            />
+        <CardContent>
+        <Typography gutterBottom variant="h5">{place.name}</Typography>
+        
+</CardContent>
+        <CardActions disableSpacing>
+        <ExpandMore
+          expand={expanded}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </ExpandMore>
+      </CardActions>
 
-            <h1>{place.name}</h1>
 
 
-        </div>
+
+<Collapse in={expanded} timeout="auto" unmountOnExit>
+ <CardContent>
+        
+
+        </CardContent>
+
+      
+      </Collapse>
+    </Card>
+        
     );
 } 
 
