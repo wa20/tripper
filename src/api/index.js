90b1b7602b1885b4be462030257815d6c6da79ec
wael <78425538+wa20@users.noreply.github.com>
2021-09-28 12:22:33 +0100

    API: made api search dynamic amending it so that type selection updates the API search

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,12 @@
 import axios from "axios";
 // require("dotenv").config();
 
-const URL =
-  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
 
 
-
-export const getData = async (sw, ne) => {
+export const getData = async (type, sw, ne) => {
   try {
-    const {data: { data }} = await axios.get(URL, {
+    const {data: { data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
         
         params: {
             bl_latitude: sw.lat,
